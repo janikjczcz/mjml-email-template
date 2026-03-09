0ef2c6a18cbd19b228e13f15e1e0f7f7f41c7921
Jaroslav Janik <jaroslav.janik@WMCM737.local>
2026-03-09 10:33:09 +0100

    add files

---
+++ build.js
@@ -0,0 +1,32 @@
+// const mjml = require('mjml')
+// const fs = require('fs')
+
+// const input = fs.readFileSync('./src/index.mjml', 'utf8')
+// const output = mjml(input, { minify: true })
+
+// fs.writeFileSync('./dist/email.html', output.html)
+
+const mjml = require('mjml')
+const fs = require('fs')
+const path = require('path')
+
+const inputPath = './src/index.mjml'
+const outputPath = './dist/email.html'
+
+function build() {
+  const input = fs.readFileSync(inputPath, 'utf8')
+  const output = mjml(input, { minify: false })
+  fs.writeFileSync(outputPath, output.html)
+  console.log(`[${new Date().toLocaleTimeString()}] Compiled ${inputPath} → ${outputPath}`)
+}
+
+// Initial build
+build()
+
+// Watch files in src/ recursively
+fs.watch('./src', { recursive: true }, (eventType, filename) => {
+  if (filename.endsWith('.mjml')) {
+    console.log(`[${new Date().toLocaleTimeString()}] Detected change in: ${filename}`)
+    build()
+  }
+})
